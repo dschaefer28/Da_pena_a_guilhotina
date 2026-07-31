6f2cc4bdfac11314d7f8878d9da3c88e66e83901
Allan Rafael <105527923+izzingly@users.noreply.github.com>
2026-07-31 10:23:47 -0300

    ajustes

---
+++ Unity-DaPenaAGuilhotina/Assets/Scripts/MenuParallax.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MenuParallax : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = .3f;
+
+    private Vector2 startPosition;
+    private Vector3 velocity;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        startPosition = transform.position;   
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+    }
+}
